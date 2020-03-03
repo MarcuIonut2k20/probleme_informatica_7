121a6e1115d90a9647f29e0353a0b7e49ea08e29
MarcuIonut2k20 <61748787+MarcuIonut2k20@users.noreply.github.com>
2020-03-03 18:24:05 +0200

    Add files via upload

---
+++ pr_2_13.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"Marcu Ionut"<<'\n';
+   cout<<"Clasa VII-a C - diriginte prof. Tivd[ Sorin"<<'\n';
+   cout<<"Scoala Gimnazială Iorgu Iordan Tecuci"<<'\n';
+
+}
